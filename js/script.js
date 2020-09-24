9575bb6301a82f46d8167aed8b21a9232f0933f5
Jorge L. Cossi <jcos201@gmail.com>
2020-09-23 22:58:15 -0400

    added styling and image

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 //alert('JS is loaded!');
+// IPO Input - Process - Output
+
+// Declare your constants and variables (State)
+let year;
+
+
+//Select and cache DOM element references
+const yearEl = document.getElementById('year');
+
+// Initialize your even listeners
+
+// Define application functions
+
+init();  //run the function as soon as the page loads;
+
+function init(){
+    year = new Date().getFullYear();
+    yearEl.innerText = year;
+}
